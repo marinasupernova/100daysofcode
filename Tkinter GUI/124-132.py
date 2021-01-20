9c8f69f87980543cecca7dd6ddd52239dcdfe0d8
marinasupernova <marina.de.suvorova@gmail.com>
2021-01-20 12:38:25 +0100

    124-132 Tkinter

---
+++ Tkinter GUI/124-132.py
@@ -0,0 +1,93 @@
+#from tkinter import *
+
+# window = Tk()
+# window.title("Window Title")
+# window.geometry("450x100")
+
+# label = Label(text = "Enter a number: ")
+
+# entry_box = Entry (text = 0)
+
+# output_box = Message(text = 0)
+
+
+# output_box ["bg"] = "red"
+
+# output_box ["fg"] = "white"
+
+# output_box ["relief"] = "sunken"
+
+# list_box = Listbox()
+
+# entry_box ["justify"] = "center"
+
+# button1 = Button(text = "click here", command = click)
+
+# label.place(x = 50, y = 20, width = 100, height = 25)
+
+# entry_box.delete(0, END)
+
+# num = entry_box.get()
+
+# answer = output_txt["text"]
+
+# output_txt["text"] = total
+
+# window.mainloop()
+ #widget what is it
+
+
+# from tkinter import *
+
+# window = Tk()
+# window.title("Its my py")
+# window.geometry("450x100")
+
+# label = Label(window, text = "Hello world")
+
+# to put things on the window - to make size as it is
+
+# label.pack()
+
+# specifies where text will be placed in the window
+# label.place(x = 50, y = 20, width = 100, height = 25)
+
+# entry_box = Entry (text = 0)
+# name = entry_box.get()
+
+# window.mainloop()
+
+'''Task 124 '''
+
+from tkinter import *
+
+# def click():
+#     name = textbox1.get()
+#     message = str("Hello " + name)
+#     textbox2["bg"] = "yellow"
+#     textbox2["fg"] = "blue"
+#     textbox2["text"] = message
+
+# window = Tk()
+# window.geometry("500x200")
+
+# label = Label(text = "What is your name?")
+# label.place(x = 30, y = 20)
+
+# textbox1 = Entry(text = "")
+# textbox1.place(x = 150, y = 20, width = 200, height = 25)
+# textbox1["justify"] = "center"
+# textbox1.focus()
+
+# button1 = Button(text = "Press me", command = click)
+# button1.place(x = 30, y = 50, width = 120, height = 25)
+
+
+# textbox2 = Message(text = "")
+# textbox2.place(x = 150, y = 50, width = 200, height = 25)
+# textbox2["bg"] = "white"
+# textbox2["fg"] = "black"
+
+
+
+# window.mainloop()
