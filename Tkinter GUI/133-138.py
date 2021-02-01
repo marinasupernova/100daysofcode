57342e88d640a0acea65fabbca8e5529dc11779b
marinasupernova <marina.de.suvorova@gmail.com>
2021-02-01 17:02:21 +0100

    tkinter chat

--- Tkinter GUI/133-138.py
+++ Tkinter GUI/133-138.py
@@ -177,48 +177,136 @@ Task 136
 
 '''
 
-from tkinter import *
+# from tkinter import *
 
-def add_to_listbox():
+# def add_to_listbox():
 
-    name = enter_name.get()
-    enter_name.delete(0, END)
+#     name = enter_name.get()
+#     enter_name.delete(0, END)
     
-    gender = select_gender.get()
-    select_gender.set("Select gender")
+#     gender = select_gender.get()
+#     select_gender.set("Select gender")
 
-    new_data = name + "," + gender + "\n"
-    names_list.insert(END, new_data)
-    enter_name.focus()
+#     new_data = name + "," + gender + "\n"
+#     names_list.insert(END, new_data)
+#     enter_name.focus()
 
+
+
+# window = Tk()
+# window.title("Name + gender")
+# window.geometry("500x500")
+
+# label_name = Label(text = "Enter a name: ")
+# label_name.place(x = 30, y = 20, width = 100, height = 25)
+
+# enter_name = Entry(text = "")
+# enter_name.place (x = 150, y = 20, width = 100, height = 25)
+
+# select_gender = StringVar(window)
+# select_gender.set("Select gender")
+
+# gender_list = OptionMenu(window, select_gender, "Man", "Woman", "Other")
+# gender_list.place(x = 30, y = 70)
+
+
+# button_add = Button(text = "Add", command = add_to_listbox)
+# button_add.place(x = 175, y = 70)
+
+
+# names_list = Listbox()
+# names_list.place(x = 30, y = 120, width = 130, height = 80)
+
+
+
+# window.mainloop()
+
+
+
+'''
+from tkinter import *
 
 
 window = Tk()
-window.title("Name + gender")
-window.geometry("500x500")
-
-label_name = Label(text = "Enter a name: ")
-label_name.place(x = 30, y = 20, width = 100, height = 25)
-
-enter_name = Entry(text = "")
-enter_name.place (x = 150, y = 20, width = 100, height = 25)
-
-select_gender = StringVar(window)
-select_gender.set("Select gender")
-
-gender_list = OptionMenu(window, select_gender, "Man", "Woman", "Other")
-gender_list.place(x = 30, y = 70)
+window.title("Chatter")
 
 
-button_add = Button(text = "Add", command = add_to_listbox)
-button_add.place(x = 175, y = 70)
+messages_frame = Frame(window)
+my_msg = StringVar()
+
+my_msg.set("Type yyour messages here")
+
+scrollbar = Scrollbar(messages_frame)
+
+msg_list = Listbox(messages_frame, height = 15, width = 50, yscrollcommand = scrollbar.set)
+
+scrollbar.pack(side = RIGHT, fill = Y)
+msg_list.pack(side = LEFT, fill = BOTH)
+msg_list.pack()
+messages_frame.pack()
 
 
-names_list = Listbox()
-names_list.place(x = 30, y = 120, width = 130, height = 80)
+entry_box = Entry(window, textvariable = my_msg)
+entry_box.bind("<Return>", send)
+entry_box.pack()
 
+send_button = Button(window, text = "Send",command = send)
+send_button.pack()
+
+window.protocol("WM_DELETE_WINDOW", on_closing)
 
 
 window.mainloop()
+'''
+
+from tkinter import *
+import csv
+
+def enter_save_name(): 
+     name = name_entrybox.get()
+     chat_body.insert(END, name + ":")
+     name_entrybox.delete(0, END)
+     file = open("chat_main.csv", "a")
+     newrecord = name + ":"
+     file.write(newrecord)
+     file.close()
 
 
+def send_save_msg():
+    msg = message_entrybox.get()
+    chat_body.insert(END, msg + "\n")
+    message_entrybox.delete(0, END)
+
+    file = open("chat_main.csv", "a")
+    newrecord = msg + "\n"
+    file.write(newrecord)
+    file.close()
+
+
+window = Tk()
+window.title("Chat")
+window.geometry("500x500")
+
+label_name = Label(text = "Enter your name")
+label_name.place (x = 30, y = 30) 
+
+name_entrybox = Entry(text = "")
+name_entrybox.place(x = 30, y = 50, width = 100, height = 25)
+
+enter_button = Button(text = "Enter", command = enter_save_name)
+enter_button.place(x = 300, y = 50) 
+
+label_msg = Label(text = "Enter your message")
+label_msg.place (x = 30, y = 290) 
+
+chat_body = Listbox()
+chat_body.place(x = 30, y = 80, width = 300, height = 200)
+
+message_entrybox = Entry(text = "")
+message_entrybox.place(x =30, y = 320, width = 300, height = 60)
+
+send_button = Button(text = "Send", command = send_save_msg)
+send_button.place(x = 350, y = 320) 
+
+
+window.mainloop()
